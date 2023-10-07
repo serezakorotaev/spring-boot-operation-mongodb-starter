822499e046eeee43b503aa8fe5034134f764661e
sykorotaye <sykorotaye@mts.ru>
2023-10-07 22:34:25 +0300

    created new starter with search logic and linked with operation-core

--- src/main/java/ru/sergkorot/dynamic/OperationProcessorMongoDbAutoConfiguration.java
+++ src/main/java/ru/sergkorot/dynamic/OperationProcessorMongoDbAutoConfiguration.java
@@ -1,5 +1,8 @@
 package ru.sergkorot.dynamic;
 
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+@AutoConfiguration
 public class OperationProcessorMongoDbAutoConfiguration {
 }
