e8c1278414bd1cf9cc78013deecd027af115ee45
sykorotaye <sykorotaye@mts.ru>
2023-10-08 21:57:30 +0300

    update main class description

--- src/main/java/ru/sergkorot/dynamic/OperationProcessorMongoDbAutoConfiguration.java
+++ src/main/java/ru/sergkorot/dynamic/OperationProcessorMongoDbAutoConfiguration.java
@@ -3,6 +3,10 @@ package ru.sergkorot.dynamic;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 
+/**
+ * @author Sergey Korotaev
+ * Main library class
+ */
 @AutoConfiguration
 public class OperationProcessorMongoDbAutoConfiguration {
 }
